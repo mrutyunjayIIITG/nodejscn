e5d65e8f527d56ac2116a19bb06f07f0aaef0b81
mrutyunjay <rout.mrutyunjay@gmail.com>
2023-08-07 10:25:38 -0400

    contact list cn

--- index.js
+++ index.js
@@ -7,12 +7,33 @@ app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'))
 
 
-app.get('/',(req,res)=>{
-    return res.render('home',{title:"my contact list"});
-})
+// app.get('/',(req,res)=>{
+//     return res.render('home',{title:"my contact list"});
+// })
+
+var contactList=[
+    {
+        name:"aman",
+        phone:"11111111111"
+    },
+    {
+        name:"badal",
+        phone:"2222222222"
+    },
+    {
+        name:"cyrus",
+        phone:"33333333333"
+    }
+]
+
+
 
 app.get('/pratice',(req,res)=>{
     return res.render('pratice',{title: "pratice"})
+})
+
+app.get('/',(req,res)=>{
+    return res.render('pratice',{title: "contactname",contact_list: contactList})
 })
 
 const port=8000;
